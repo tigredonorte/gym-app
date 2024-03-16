747121b620e2ec68595653168610d53083b38881
Thompson <thompson@clickpart.co>
2024-03-16 16:36:38 -0300

    created a shared library with context to share env variables inejected on applications

---
+++ libs/shared/web/src/lib/EnvContext.tsx
@@ -0,0 +1,29 @@
+import React, { createContext, useContext } from 'react';
+
+type Env = Record<string, string | number>;
+
+interface EnvProviderProps {
+  children: React.ReactNode;
+  env: Env;
+}
+
+export const EnvContext = createContext<Env>({});
+
+export const EnvProvider: React.FC<EnvProviderProps> = ({ children, env }) => {
+  if (!env) {
+    env = {};
+  }
+  return (
+    <EnvContext.Provider value={env}>
+      {children}
+    </EnvContext.Provider>
+  )
+};
+
+export const useEnv = (): Env => {
+  const context = useContext(EnvContext);
+  if (!context) {
+    throw new Error('useEnv must be used within an EnvProvider');
+  }
+  return context;
+};
