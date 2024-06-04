345b4f8a4c18d70f90fa9005b30b1b31cb4bd41a
Thompson <thompson@clickpart.co>
2024-06-04 07:02:18 -0300

    adding icons and default layout system

--- apps/gym-web-app/src/app/app.tsx
+++ apps/gym-web-app/src/app/app.tsx
@@ -2,12 +2,17 @@ import { EnvProvider } from '@gym-app/shared/web';
 import React from 'react';
 import { environment } from '../environments/environment';
 import { AppRouter } from './app.router';
+import { ThemeProvider, createTheme } from '@mui/material';
 
 const App: React.FC = () => {
+  const defaultTheme = createTheme();
+
   return (
-    <EnvProvider env={environment}>
-      <AppRouter />
-    </EnvProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <EnvProvider env={environment}>
+        <AppRouter />
+      </EnvProvider>
+    </ThemeProvider>
   )
 }
 
