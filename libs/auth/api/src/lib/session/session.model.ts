e05003883a5032610791c5dd42f6d3ecaa7676e6
Thompson <thompson@clickpart.co>
2024-06-07 08:20:46 -0300

    adding session models

---
+++ libs/auth/api/src/lib/session/session.model.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { IRequestInfo } from '../request-info-middleware';
+import { AccessLogSchema } from './accessLog.model';
+import { AccessLog } from './session.dto';
+
+@Schema({ timestamps: true })
+export class Session {
+  @Prop({ required: true })
+  userId!: string;
+
+  @Prop({ type: Object })
+  deviceInfo!: IRequestInfo['userData']['deviceInfo'];
+
+  @Prop({ required: true })
+  sessionId!: string;
+
+  @Prop({ default: 'active' })
+  status: 'active' | 'inactive' = 'active';
+
+  @Prop({ type: [AccessLogSchema], default: [] })
+  access!: AccessLog[];
+}
+
+export type SessionDocument = Session & Document;
+export const SessionSchema = SchemaFactory.createForClass(Session);
