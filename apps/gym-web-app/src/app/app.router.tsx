3209c135825b6f2fdc2f8b2ffc45765bdc8a8ad8
Thompson <thompson@clickpart.co>
2024-06-10 07:56:39 -0300

    integrates a header menu on web app

--- apps/gym-web-app/src/app/app.router.tsx
+++ apps/gym-web-app/src/app/app.router.tsx
@@ -1,7 +1,9 @@
+import { AuthPath, AuthRouter } from '@gym-app/auth/web';
+import { Header } from '@gym-app/ui-menu';
 import React from 'react';
 import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
-import { AuthPath, AuthRouter } from '@gym-app/auth/web';
 import { UserPath, UserRouter } from '../modules/user/User.router';
+import { drawerMenuItemList, menuItems } from './menuItems';
 
 const isAuthenticated = () => {
   const userData = localStorage.getItem('userData');
@@ -21,10 +23,13 @@ export const AppRouter: React.FC = () => {
   }
 
   return (
-    <Routes>
-      <Route path={`/${UserPath}/*`} element={<UserRouter />} />
-      <Route path="*" element={<Navigate to={`/${UserPath}`} />} />
-    </Routes>
+    <>
+      <Header siteTitle="Gym App" menuItems={menuItems} drawerMenuItemList={drawerMenuItemList} />
+      <Routes>
+        <Route path={`/${UserPath}/*`} element={<UserRouter />} />
+        <Route path="*" element={<Navigate to={`/${UserPath}`} />} />
+      </Routes>
+    </>
   );
 }
 
