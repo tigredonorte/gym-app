9f0598bd3c354e645d6d48923e511868a39cb9f9
Thompson <thompson@clickpart.co>
2024-06-10 07:56:39 -0300

    integrates a header menu on web app

---
+++ apps/gym-web-app/src/app/menuItems.ts
@@ -0,0 +1,18 @@
+import { DrawerMenuItem, IMenuItem } from "@gym-app/ui-menu";
+// import { mdiAccount, mdiHome } from "@mdi/js";
+
+export const menuItems: IMenuItem[] = [
+  // it will be implemented soon
+  // {
+  //   id: 'notifications',
+  //   icon: mdiBell,
+  //   count: 0,
+  //   label: "Notifications",
+  //   ariaLabel: "show new notifications"
+  // }
+]
+
+export const drawerMenuItemList: DrawerMenuItem[] = [
+  // { icon: mdiHome, text: 'Home', path: '/' },
+  // { icon: mdiAccount, text: 'Profile', path: '/profile' },
+];
