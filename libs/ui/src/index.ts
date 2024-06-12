8ccce341bd6a19c36eeb6091fc0de4917250edbc
Thompson <thompson@clickpart.co>
2024-06-12 06:28:37 -0300

    adding ui lib with card and page header

---
+++ libs/ui/src/index.ts
@@ -0,0 +1,4 @@
+export * from './lib/CardHeader';
+export * from './lib/PageHeader';
+export * from './lib/utils';
+export * from './lib/menu';
---
+++ libs/ui/src/lib/utils/calcLayoutHeight.ts
@@ -0,0 +1,15 @@
+const heights: { [key: string]: number } = {
+	header: 152,
+	userHeader: 74,
+	nav: 78,
+	mobileHeader: 194,
+	footer: 427,
+};
+
+export function calcLayoutHeight(type: string): number {
+	return heights[type] || 148;
+}
+
+export function calcLayoutHeightPx(type: string): string {
+  return `${calcLayoutHeight(type)}px`;
+}
