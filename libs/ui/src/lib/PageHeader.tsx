0a536be417675e8372bcd7844d5fc7b4e3ce35af
Thompson <thompson@clickpart.co>
2024-06-12 06:28:37 -0300

    adding ui lib with card and page header

---
+++ libs/ui/src/lib/PageHeader.tsx
@@ -0,0 +1,37 @@
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { SxProps } from '@mui/system';
+
+interface PageHeaderProps {
+	children?: React.ReactNode;
+	title: string;
+	sx?: SxProps;
+}
+
+export const PageHeader: React.FC<PageHeaderProps> = ({ children, title, sx = {} }: PageHeaderProps) => {
+	return (
+		<Stack
+			component="header"
+			my={3}
+			direction="row"
+			justifyContent="space-between"
+			alignItems="center"
+			flexWrap="wrap"
+			textTransform="uppercase"
+			sx={sx}
+		>
+			<Typography
+				variant="h5"
+				fontSize={18}
+				textTransform="inherit"
+				borderLeft={5}
+				borderColor="primary.400"
+				pl={1.5}
+				height="fit-content"
+			>
+				{title}
+			</Typography>
+			{children}
+		</Stack>
+	);
+};
