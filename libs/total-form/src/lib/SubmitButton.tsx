7a6374e8c39ea6a87f3fe262e77fab27508f80de
Thompson <thompson@clickpart.co>
2024-06-24 07:58:26 -0300

    adding more options on submit button

--- libs/total-form/src/lib/SubmitButton.tsx
+++ libs/total-form/src/lib/SubmitButton.tsx
@@ -6,6 +6,11 @@ interface SubmitButtonProps {
   title?: string;
   color?: 'primary' | 'secondary';
   variant?: 'contained' | 'outlined' | 'text';
+  className?: string;
+  fullWidth?: boolean;
+  endIcon?: React.ReactNode;
+  startIcon?: React.ReactNode;
+  loadingIcon?: React.ReactNode | string;
 }
 
 export const SubmitButton: React.FC<SubmitButtonProps & ButtonOwnProps> = ({ 
@@ -13,15 +18,28 @@ export const SubmitButton: React.FC<SubmitButtonProps & ButtonOwnProps> = ({
   title,
   variant= "contained",
   color="primary",
+  fullWidth = true,
+  endIcon,
+  startIcon,
+  loadingIcon = '...',
   ...props
 }) => {
-  
   const { isFormValid, isRequesting } = useFormContext();
   return (
-    <Button type="submit" fullWidth color={color} variant={variant} disabled={!isFormValid || isRequesting} sx={{ mt: 3, mb: 2 }} {...props}>
+    <Button
+      {...props}
+      type="submit"
+      fullWidth={fullWidth}
+      color={color}
+      variant={variant}
+      disabled={!isFormValid || isRequesting}
+      sx={{ mt: 3, mb: 2 }}
+    >
+      {startIcon && (<span style={{ marginRight: 5, display: 'flex' }}>{startIcon}</span>)}
       {children}
       {title}
-      {isRequesting && '...'}
+      {endIcon && (<span style={{ marginLeft: 5, display: 'flex' }}>{endIcon}</span>)}
+      {isRequesting && loadingIcon}
     </Button>
   )
 };
