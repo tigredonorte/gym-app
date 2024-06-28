7b26e76aaf420c8da0d5d5a92d039cc7625ff42f
Thompson <thompson@clickpart.co>
2024-06-28 07:48:39 -0300

    56 - author guard created and protecting user.controller

--- libs/user/api/src/lib/guards/jwt-guard.ts
+++ libs/user/api/src/lib/guards/jwt-guard.ts
@@ -1,7 +1,7 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
-import { User } from './user.model';
+import { User } from '../user.model';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
--- libs/user/api/src/lib/user.controller.ts
+++ libs/user/api/src/lib/user.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, HttpCode, HttpStatus, Param, Post, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from './jwt-guard';
+import { AuthorGuard, JwtAuthGuard } from './guards';
 import { UpdateUserDto } from './user.dto';
 import { User } from './user.model';
 import { UserService } from './user.service';
 
 @Controller('user')
 @UseGuards(JwtAuthGuard)
+@UseGuards(AuthorGuard)
 export class UserController {
 
   constructor(private userService: UserService) {}
