5f6869ff83c9029a5ea55f1d545bd07bdddbf5dc
Thompson <thompson@clickpart.co>
2024-07-06 14:34:21 -0300

    feat: adding confirm dialog

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -3,5 +3,6 @@ export * from './lib/CrudContainer';
 export * from './lib/PageHeader';
 export * from './lib/Settings';
 export * from './lib/menu';
+export * from './lib/modal';
 export * from './lib/utils';
 
