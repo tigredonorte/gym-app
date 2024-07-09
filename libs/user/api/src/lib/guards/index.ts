a90dbabf56be20db4a49ce95e03eeb029e40db02
Thompson <thompson@clickpart.co>
2024-07-09 07:53:00 -0300

    adding public decorator to bypass jwt guard

--- libs/user/api/src/lib/guards/index.ts
+++ libs/user/api/src/lib/guards/index.ts
@@ -1,2 +1,3 @@
 export * from './jwt-guard';
 export * from './author-guard';
+export * from './public.decorator';
--- libs/user/api/src/lib/guards/jwt-guard.ts
+++ libs/user/api/src/lib/guards/jwt-guard.ts
@@ -1,15 +1,27 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService, TokenExpiredError } from '@nestjs/jwt';
 import { Request } from 'express';
 import { User } from '../user.model';
 import { extractTokenFromHeader } from './extractTokenFromHeader';
+import { IS_PUBLIC_KEY } from './public.decorator';
 
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly reflector: Reflector
+  ) {}
 
   canActivate(context: ExecutionContext): boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
     const request = context.switchToHttp().getRequest<Request & { user: User }>();
     const token = extractTokenFromHeader(request);
     if (!token) {
