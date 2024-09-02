3b2f921e7aac58737b87df18a9e1ef23493a8b04
Thompson <thompson@clickpart.co>
2024-09-02 08:18:30 -0300

    adding closeButton on ui folder

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,8 +1,9 @@
+export * from './lib/Button';
 export * from './lib/Cards';
 export * from './lib/CrudContainer';
-export * from './lib/PageHeader';
-export * from './lib/Settings';
 export * from './lib/menu';
 export * from './lib/modal';
+export * from './lib/PageHeader';
+export * from './lib/Settings';
 export * from './lib/utils';
 
